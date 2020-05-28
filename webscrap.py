d79476d68b7ab775f949b18e0fdd1f5d845eb975
anoopk1981 <36709038+anoopk1981@users.noreply.github.com>
2020-05-27 21:28:31 -0400

    Update webscrap.py

--- webscrap.py
+++ webscrap.py
@@ -1,4 +1,6 @@
 import scrapy
+import re
+import pandas as pd
 
 
 class DmartSpider(scrapy.Spider):
@@ -7,14 +9,30 @@ class DmartSpider(scrapy.Spider):
     start_urls = ['http://www.dmart.in/vegetables']
 
     def parse(self, response):
-        category = response.xpath("//a[@class='nonrwd_only_bc']/text()").get()
-        items = response.xpath("//h4//a/text()").getall()
-        cost = response.xpath("//p[@class='product-listing--discounted-price ']/text()").getall()
+        category = response.xpath("//a[@class='nonrwd_only_bc']/text()").get() # Get the category - In this case it is Vegetables
+        category = re.sub(r'\s', '', category)  #Remove the unwanted characters
+        category = re.sub("[\(\[].*?[\)\]]", "", category) #Remove text inside brackets along with the brackets itself
+        items = response.xpath("//h4//a/text()").getall()  #Get the items
 
+        for idx, item in enumerate(items):
+                item = re.sub("[\(\[].*?[\)\]]", "", item)  #Remove text inside brackets along with the brackets itself
+                item = item.replace("Fresh", "") #Remove the work Fresh
+                item = item.strip()
+                items[idx] = item
+        tuple_list =  [tuple(item.split(":")) for item in items] #Convert the item list into a tuple
+        item, quantity = zip(*tuple_list) #Separate the tuple into two lists - one to store the item name and next one to store the quantity
 
+        cost = response.xpath("//p[@class='product-listing--discounted-price ']/text()").getall()  #Get the cost
+        cost = list(dict.fromkeys(cost))  #Remove word 'Dmart' as it is duplicated
+        cost.remove('DMart ') #Remove the final occurence of the word
 
-        yield {
-            'category': category,
-            'items': items,
-            'cost':cost
-        }
+        categories = [category for _ in range(len(cost))] #Create a list to store the category value. The length will be the length of the cost list or item list or quantity list
+
+        df = pd.DataFrame(columns=['Category', 'Item','Quantity','Cost'])  #Create data frame
+        df['Category'] = categories
+        df['Item'] = item
+        df['Quantity'] = quantity
+        df['Cost'] = cost
+
+        yield df.to_csv("data.csv", sep=",")  #Write to a csv file
+
